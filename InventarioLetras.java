11504fd1461cc96b85bd8d307fda6eb3938bd952
cisternna <105678565+cisternna@users.noreply.github.com>
2026-05-06 23:46:38 -0400

    Add files via upload
    Base del proyecto, aún nada de lógica.

---
+++ InventarioLetras.java
@@ -0,0 +1,45 @@
+public class InventarioLetras{
+    
+    // atributos
+    // constructor
+
+    public InventarioLetras(String data) {
+        
+    }
+    public char encriptarCesar(char letra) {
+        
+    }
+    public char desencriptarCesar(char letra) {
+
+    }
+    public String encriptarPalabra(String palabra, int desplazamiento) {
+
+    }
+    public String desencriptarPalabra(String palabra, int desplazamiento) {
+
+    }
+    public int get(char letra) {
+
+    }
+    public void set(char letra, int valor) {
+
+    }
+    public int size() {
+
+    }
+    public boolean isEmpty() {
+
+    }
+    public String toString() {
+
+    }
+    public InventarioLetras add(InventarioLetras otro) {
+
+    }
+    public InventarioLetras amplifies(int n) {
+
+    }
+    public InventarioLetras subtract(InventarioLetras otro) {
+
+    }
+}
